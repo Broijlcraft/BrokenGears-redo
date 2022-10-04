be272c2eca4f0c8912aa4de53e485d44cfaf4064
Max <max.broijl@gmail.com>
2022-10-04 16:44:47 +0200

    Adding variables to script Tile

--- BrokenGears-redo/Assets/Broken Gears/Scripts/Tile.cs
+++ BrokenGears-redo/Assets/Broken Gears/Scripts/Tile.cs
@@ -2,6 +2,11 @@ namespace BrokenGears {
     using UnityEngine;
 
     public class Tile : MonoBehaviour {
+        [SerializeField] private Tile child;
+        [SerializeField] private Tile parent;
 
+        public Tile Child => child;
+        public Tile Parent => parent;
+        public bool IsOccupied { get; set; }
     }
 }
