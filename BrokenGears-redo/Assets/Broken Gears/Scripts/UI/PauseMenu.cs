43905fceec038f988a3188d2f3aa43ab1d53ec2b
Max <max.broijl@gmail.com>
2022-10-07 17:03:25 +0200

    Adding script PauseMenu

---
+++ BrokenGears-redo/Assets/Broken Gears/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,40 @@
+namespace BrokenGears.UI {
+    using System.Collections;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using UnityEngine.Events;
+
+    public class PauseMenu : MonoBehaviour {
+        public UnityEvent<bool> OnPauseChanged = new UnityEvent<bool>();
+
+        public static PauseMenu Instance { get; private set; }
+        public bool IsPaused { get; private set; }
+
+        private void Awake() {
+            if (Instance) {
+                Destroy(this);
+                return;
+            }
+            Instance = this;
+        }
+
+        private void Start() {
+            OnPauseChanged.AddListener(OnPause_Internal);
+        }
+
+        private void Update() {
+            if (Input.GetButtonDown("Cancel")) {
+                Pause(!IsPaused);
+            }
+        }
+
+        public void Pause(bool pause) {
+            IsPaused = pause;
+            OnPauseChanged?.Invoke(pause);
+        }
+
+        private void OnPause_Internal(bool value) {
+            Time.timeScale = value ? 0f : 1f;
+        }
+    }
+}
